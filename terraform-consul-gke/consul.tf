172ef3c09b7420516977119bf1796a524a816433
Jacob Mammoliti <jacob.mammoliti@arctiq.ca>
2020-06-10 10:12:47 -0400

    commenting out app and consul install for tfe

--- terraform-consul-gke/consul.tf
+++ terraform-consul-gke/consul.tf
@@ -1,24 +1,24 @@
-resource "kubernetes_namespace" "consul" {
-  metadata {
-    name = var.consul_namespace
-  }
-}
+# resource "kubernetes_namespace" "consul" {
+#   metadata {
+#     name = var.consul_namespace
+#   }
+# }
 
-resource "helm_release" "consul" {
-  name      = "consul"
-  chart     = "consul-helm"
-  namespace = kubernetes_namespace.consul.metadata.0.name
+# resource "helm_release" "consul" {
+#   name      = "consul"
+#   chart     = "consul-helm"
+#   namespace = kubernetes_namespace.consul.metadata.0.name
 
-  values = [
-    templatefile("${path.module}/consul/values.tmpl", {
-      datacenter                       = var.consul_datacenter,
-      image_tag                        = var.consul_image_tag,
-      enable_bootstrap_acls            = var.consul_enable_bootstrap_acls,
-      replicas                         = var.initial_node_count,
-      ui_enabled                       = var.consul_ui_enabled,
-      service_type                     = var.consul_service_type,
-      connect_injected_enabled         = var.consul_connect_injected_enabled,
-      connect_injected_enabled_default = var.consul_connect_injected_enabled_default
-    })
-  ]
-}
+#   values = [
+#     templatefile("${path.module}/consul/values.tmpl", {
+#       datacenter                       = var.consul_datacenter,
+#       image_tag                        = var.consul_image_tag,
+#       enable_bootstrap_acls            = var.consul_enable_bootstrap_acls,
+#       replicas                         = var.initial_node_count,
+#       ui_enabled                       = var.consul_ui_enabled,
+#       service_type                     = var.consul_service_type,
+#       connect_injected_enabled         = var.consul_connect_injected_enabled,
+#       connect_injected_enabled_default = var.consul_connect_injected_enabled_default
+#     })
+#   ]
+# }
